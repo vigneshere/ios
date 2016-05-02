62a7e8ebdc4bb5703d0efc6f3e1d2c7b66e2296b
Vignesh Saravanai <vigneshere@gmail.com>
2016-05-02 23:45:36 +0530

    - random number generator   - added new .ConstOperation type - number formatter for better formatting - more operations supported - backspace operation support added - showing history operations in a separate label

--- Calculator/Calculator/CalculatorBrain.swift
+++ Calculator/Calculator/CalculatorBrain.swift
@@ -13,12 +13,16 @@ class CalculatorBrain {
     private var accumulator = 0.0
     
     func setOperand(operand: Double) {
+        if pendingBinaryOperationInfo.status == false {
+            internalState.removeAll()
+        }
         internalState.append(operand)
         accumulator = operand
     }
     
     private enum Operation {
         case Constant(Double)
+        case ConstOperation(() -> Double)
         case UnaryOperation((Double) -> Double)
         case BinaryOperation((Double, Double) -> Double)
         case Equals
@@ -26,32 +30,48 @@ class CalculatorBrain {
     
     private var operations : Dictionary<String, Operation> = [
         "π" : Operation.Constant(M_PI),
+        "℮" : Operation.Constant(M_E),
+        "R" : Operation.ConstOperation({ drand48() } ),
         "±" : Operation.UnaryOperation( { -$0 } ),
+        "√" : Operation.UnaryOperation( { sqrt($0) }  ),
         "×" : Operation.BinaryOperation( { $0 * $1 } ),
         "−" : Operation.BinaryOperation( { $0 - $1 } ),
         "+" : Operation.BinaryOperation( { $0 + $1 } ),
         "÷" : Operation.BinaryOperation( { $0 / $1 } ),
         "%" : Operation.BinaryOperation( { $0 % $1 } ),
+        "X^Y" : Operation.BinaryOperation( { pow($0 , $1) }  ),
         "=" : Operation.Equals
     ]
     
     func clear() {
-        pendingBinaryOperation = false
+        pendingBinaryOperationInfo.status = false
         accumulator = 0.0
         internalState.removeAll()
     }
     
     func performOperation(symbol: String) {
         if let operation = operations[symbol] {
+           //user changed his mind and tries different binaryOperator now
+           if pendingBinaryOperationInfo.status == true,
+                let prevSymbol = internalState.last as? String,
+                let prevOperation = operations[prevSymbol] {
+                switch prevOperation {
+                case .BinaryOperation:
+                    pendingBinaryOperationInfo.status = false
+                    internalState.removeLast()
+                default:break
+                }
+            }
+            
             internalState.append(symbol)
             switch operation {
             case .Constant(let value): accumulator = value
             case .UnaryOperation(let function): accumulator = function(accumulator)
             case .BinaryOperation(let function):
                 performBinaryOperation()
-                pendingBinaryOperationInfo = PendingBinaryOperationInfo(firstOperand: accumulator, binaryFunction: function)
-                pendingBinaryOperation = true
+                pendingBinaryOperationInfo = PendingBinaryOperationInfo(status:true, firstOperand: accumulator, binaryFunction: function)
             case .Equals: performBinaryOperation()
+            case .ConstOperation(let function): accumulator = function()
             }
             
         }
@@ -59,12 +79,44 @@ class CalculatorBrain {
     
     typealias PropertyList = AnyObject
     
+    var description : String {
+        get {
+            var updatedState : [String] = [String]()
+            let numberFormatter = NSNumberFormatter()
+            numberFormatter.alwaysShowsDecimalSeparator = false
+            var applyUnaryOnResult : Bool = false
+            for op in internalState {
+                if let operand = op as? Double {
+                    applyUnaryOnResult = false
+                    updatedState.append(numberFormatter.stringFromNumber(operand)!)
+                }
+                else if let symbol = op as? String {
+                    if let operation = operations[symbol] {
+                        switch operation {
+                        case .Equals:
+                            applyUnaryOnResult = true
+                        case .UnaryOperation:
+                            updatedState.insert(symbol + "(", atIndex:
+                                (applyUnaryOnResult ? 0 : (updatedState.count - 1)))
+                            updatedState.append(")")
+                            applyUnaryOnResult = true
+                        default:
+                            updatedState.append(symbol)
+                            applyUnaryOnResult = false
+                        }
+                    }
+                }
+                
+            }
+            return (updatedState.joinWithSeparator(" "))
+        }
+    }
+    
     var savedState : PropertyList {
         get {
             return internalState
         }
         set {
-            let oldPendingBinaryOp = pendingBinaryOperation
             let oldBinaryOpInfo = pendingBinaryOperationInfo
             clear()
             if let ops = newValue as? [AnyObject] {
@@ -77,7 +129,6 @@ class CalculatorBrain {
                     }
                 }
             }
-            pendingBinaryOperation = oldPendingBinaryOp
             pendingBinaryOperationInfo = oldBinaryOpInfo
         }
     }
@@ -85,16 +136,21 @@ class CalculatorBrain {
     private var internalState = [PropertyList]()
     
     private func performBinaryOperation() {
-        if pendingBinaryOperation {
-            accumulator = pendingBinaryOperationInfo!.binaryFunction(pendingBinaryOperationInfo!.firstOperand, accumulator)
-            pendingBinaryOperation = false
+        if pendingBinaryOperationInfo.status == true {
+            accumulator = pendingBinaryOperationInfo.binaryFunction(pendingBinaryOperationInfo.firstOperand, accumulator)
+            pendingBinaryOperationInfo.status = false
         }
     }
     
-    private var pendingBinaryOperation = false
-    private var pendingBinaryOperationInfo : PendingBinaryOperationInfo?
-    
+    var isPartialResult : Bool {
+        get {
+            return pendingBinaryOperationInfo.status
+        }
+    }
+
+    private var pendingBinaryOperationInfo = PendingBinaryOperationInfo(status: false, firstOperand: 0.0, binaryFunction: { $0 + $1 } )
     private struct PendingBinaryOperationInfo {
+        var status : Bool
         var firstOperand : Double
         var binaryFunction : (Double, Double) -> Double
     }
--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -20,6 +20,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    @IBOutlet weak var inputHistory: UILabel!
     @IBOutlet private weak var displayOutput: UILabel!
     
     private var displayOutputValue: Double {
@@ -30,13 +32,16 @@ class ViewController: UIViewController {
             displayOutput.text = String(newValue)
         }
     }
-    
+
     var userTyping = false
     var brain = CalculatorBrain()
     
     @IBAction private func touchDigit(sender: UIButton) {
         let digit = sender.currentTitle!
-        displayOutput.text = userTyping ? (displayOutput.text! + digit) : digit
+        let currentDisplay = userTyping ? (displayOutput.text! + digit) : digit
+        if currentDisplay == "." || Double(currentDisplay) != nil  {
+            displayOutput.text = currentDisplay
+        }
         userTyping = true
     }
     
@@ -53,11 +58,23 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBAction private func clearPanel(sender: UIButton) {
+    @IBAction private func clear(sender: UIButton) {
         savedState = nil
         brain.clear()
         displayOutputValue = brain.result
         userTyping = false
+    }
+    
+    @IBAction private func backspace() {
+        if let str = displayOutput.text where
+            (str.characters.count > 0 && userTyping) {
+            let index = str.startIndex.advancedBy(str.characters.count - 1)
+            displayOutput.text = str.substringToIndex(index)
+            if (displayOutput.text!.isEmpty) {
+                displayOutput.text = "0"
+                userTyping = false
+            }
+        }
     }
     
     @IBAction private func performOperation(sender: UIButton) {
@@ -69,7 +86,15 @@ class ViewController: UIViewController {
         
         if let symbol = sender.currentTitle {
             brain.performOperation(symbol)
-            displayOutputValue = brain.result
+            let numberFormatter = NSNumberFormatter()
+            numberFormatter.maximumFractionDigits = 6
+            numberFormatter.alwaysShowsDecimalSeparator = false
+            displayOutput.text = numberFormatter.stringFromNumber(brain.result)
+        }
+        
+        inputHistory.text = brain.description + (brain.isPartialResult ? " ..." : " ")
+        if (inputHistory.text!.isEmpty) {
+            inputHistory.text = " "
         }
     }
     
